30397eda57e1911b292f2327aec7ff8d050017ce
Adil Islambekov <osnapitzadi@gmail.com>
2020-04-07 23:50:03 -0400

    constuctor function

---
+++ main.js
@@ -0,0 +1,23 @@
+// global variables
+let aStore = [];
+let aCart = [];
+
+// store items constructor 
+function items (id, name, price, qty, maxPerCustomer, category, shippingPrice, reviews, description, image) {
+
+    this.id = id,
+    this.name = name,
+    this.price = price,
+    this.qty = qty,
+    this.maxPerCustomer = maxPerCustomer,
+    this.category = category,
+    this.shippingPrice = shippingPrice,
+    this.reviews = reviews,
+    this.description = description,
+    this.image = image
+
+}
+
+// cart item constructor 
+function cartItems (id, price, qty, shippingPrice);
+
