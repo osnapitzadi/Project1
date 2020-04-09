5ddc80730748079bb1917a40a54e872d11c98e7f
Adil Islambekov <osnapitzadi@gmail.com>
2020-04-09 02:38:48 -0400

    photo

--- main.js
+++ main.js
@@ -30,15 +30,15 @@ var immage = false;
 // Constructor Calls
 
 aStore.push(new items (1, "Nike Pro", 119.99, 1, 1, "Shoos", 4,99, [], "lorem ipsum description", "src/1.png"));
-aStore.push(new items (2, "Adidas Origin", 199.99, 10, 5, "Shoos", 4,99, [], "lorem ipsum description", "src/2.png"));
-aStore.push(new items (3, "New Balance", 159.99, 20, 5, "Shoos", 4.99, [], "lorem ipsum description", "src/3.png"));
+aStore.push(new items (2, "Adidas Origin", 199.99, 10, 5, "Shoos", 4,99, [], "lorem ipsum description", "src/2.jpg"));
+aStore.push(new items (3, "New Balance", 159.99, 20, 5, "Shoos", 4.99, [], "lorem ipsum description", "src/3.jpg"));
 aStore.push(new items (4, "Polo T-Shirt", 19.99, 25, 5, "T-Shirt", 4.99, [], "lorem ipsum description", "src/4.png"));
-aStore.push(new items (5, "T-Shirt", 21.99, 25, 5, "T-Shirt", 4.99, [], "lorem ipsum description", "src/5.png"));
+aStore.push(new items (5, "T-Shirt", 21.99, 25, 5, "T-Shirt", 4.99, [], "lorem ipsum description", "src/5.jpg"));
 aStore.push(new items (6, "Amazon T-Shirt", 9.99, 25, 5, "T-Shirt", 4.99, [], "lorem ipsum description", "src/6.png"));
-aStore.push(new items (7, "Levi's Jeans", 59.99, 25, 5, "Jeans", 4.99, [], "lorem ipsum description", "src/7.png"));
-aStore.push(new items (8, "Jack & Jones Jeans", 99.99, 25, 5, "Jeans", 4.99, [], "lorem ipsum description", "src/8.png"));
-aStore.push(new items (9, "Amazon Jeans", 12.99, 25, 5, "Jeans", 4.99, [], "lorem ipsum description", "src/9.png"));
-aStore.push(new items (10, "Champion's Hoodie", 34.99, 100, 10, "Hoodie", 4.99, [], "lorem ipsum description", "src/10.png"));
+aStore.push(new items (7, "Levi's Jeans", 59.99, 25, 5, "Jeans", 4.99, [], "lorem ipsum description", "src/7.jpg"));
+aStore.push(new items (8, "Jack & Jones Jeans", 99.99, 25, 5, "Jeans", 4.99, [], "lorem ipsum description", "src/8.jpg"));
+aStore.push(new items (9, "Amazon Jeans", 12.99, 25, 5, "Jeans", 4.99, [], "lorem ipsum description", "src/9.jpg"));
+aStore.push(new items (10, "Champion's Hoodie", 34.99, 100, 10, "Hoodie", 4.99, [], "lorem ipsum description", "src/10.jpg"));
 aStore.push(new items (11, "Naruto Hoodie", 69.99, 100, 10, "Hoodie", 4.99, [], "lorem ipsum description", "src/11.png"));
 aStore.push(new items (12, "Just White Hoodie", 15.99, 100, 10, "Hoodie", 4.99, [], "lorem ipsum description", "src/11.png"));
 aStore.push(new items (13, "White Socks", 19.99, 100, 20, "Socks", 4.99, [], "lorem ipsum description", "src/12.png"));
@@ -68,7 +68,10 @@ function displayStoreItems() {
         var tempCard = document.createElement("div");
         tempCard.className = "card";
         tempCard.style = "width: 18rem;";
-        // var cardBody = document.createElemen("IMG");
+        var cardImg = document.createElement("IMG");
+        cardImg.className = "card-img-top";
+        cardImg.setAttribute('src', tempItem.image);
+        console.log(tempItem.image);
         var cardBody = document.createElement("div");
         cardBody.className = "card-body";
         var cardTitle = document.createElement("h5")
@@ -92,6 +95,7 @@ function displayStoreItems() {
         addToCard.innerText = "Add to card"
 
         divOutput.appendChild(tempCard);
+        tempCard.appendChild(cardImg);
         tempCard.appendChild(cardBody);
         cardBody.appendChild(cardTitle);
         cardBody.appendChild(cardText);
