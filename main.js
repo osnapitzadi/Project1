b310270d1279c94972a9036f3d55a2473c53978d
vellkunio <vellkunio>
2020-04-10 15:14:47 -0400

    Photos Update

--- main.js
+++ main.js
@@ -30,15 +30,15 @@ var immage = false;
 // Constructor Calls
 
 aStore.push(new items (1, "Nike Pro", 119.99, 1, 1, "Shoos", 4.99, getRandowReviews(), "", "src/1.png"));
-aStore.push(new items (2, "Adidas Origin", 199.99, 10, 5, "Shoos", 4.99, getRandowReviews(), "", "src/2.jpg"));
-aStore.push(new items (3, "New Balance", 159.99, 20, 5, "Shoos", 4.99,  getRandowReviews(), "",  "src/3.jpg"));
+aStore.push(new items (2, "Adidas Origin", 199.99, 10, 5, "Shoos", 4.99, getRandowReviews(), "", "src/2.png"));
+aStore.push(new items (3, "New Balance", 159.99, 20, 5, "Shoos", 4.99,  getRandowReviews(), "",  "src/3.png"));
 aStore.push(new items (4, "Polo T-Shirt", 19.99, 25, 5, "T-Shirt", 4.99,  getRandowReviews(), "",  "src/4.png"));
-aStore.push(new items (5, "T-Shirt", 21.99, 25, 5, "T-Shirt", 4.99,  getRandowReviews(), "",  "src/5.jpg"));
+aStore.push(new items (5, "T-Shirt", 21.99, 25, 5, "T-Shirt", 4.99,  getRandowReviews(), "",  "src/5.png"));
 aStore.push(new items (6, "Amazon T-Shirt", 9.99, 25, 5, "T-Shirt", 4.99,  getRandowReviews(), "",  "src/6.png"));
-aStore.push(new items (7, "Levi's Jeans", 59.99, 25, 5, "Jeans", 4.99,  getRandowReviews(), "",  "src/7.jpg"));
-aStore.push(new items (8, "Jack & Jones Jeans", 99.99, 25, 5, "Jeans", 4.99,  getRandowReviews(), "",  "src/8.jpg"));
-aStore.push(new items (9, "Amazon Jeans", 12.99, 25, 5, "Jeans", 4.99,  getRandowReviews(), "",  "src/9.jpg"));
-aStore.push(new items (10, "Champion's Hoodie", 34.99, 100, 10, "Hoodie", 4.99,  getRandowReviews(), "",  "src/10.jpg"));
+aStore.push(new items (7, "Levi's Jeans", 59.99, 25, 5, "Jeans", 4.99,  getRandowReviews(), "",  "src/7.png"));
+aStore.push(new items (8, "Jack & Jones Jeans", 99.99, 25, 5, "Jeans", 4.99,  getRandowReviews(), "",  "src/8.png"));
+aStore.push(new items (9, "Amazon Jeans", 12.99, 25, 5, "Jeans", 4.99,  getRandowReviews(), "",  "src/9.png"));
+aStore.push(new items (10, "Champion's Hoodie", 34.99, 100, 10, "Hoodie", 4.99,  getRandowReviews(), "",  "src/10.png"));
 aStore.push(new items (11, "Naruto Hoodie", 69.99, 100, 10, "Hoodie", 4.99,  getRandowReviews(), "",  "src/11.png"));
 aStore.push(new items (12, "Just White Hoodie", 15.99, 100, 10, "Hoodie", 4.99,  getRandowReviews(), "",  "src/12.png"));
 aStore.push(new items (13, "White Socks", 19.99, 100, 20, "Socks", 4.99,  getRandowReviews(), "",  "src/13.png"));
